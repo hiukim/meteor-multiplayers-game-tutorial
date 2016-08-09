55d84fdc05029c49043435f9eb1dcd7d943a1900
hiukim <hiukim@gmail.com>
2016-08-09 19:28:55 +0800

    bug fix: should publish finished games as well

--- imports/api/server/publications.js
+++ imports/api/server/publications.js
@@ -5,7 +5,7 @@ Meteor.publish('games', function() {
   // access control: only for loggined-in users
   if (this.userId) { // this.userId is the id of the currently loggined in user
     // filtering: only games with WAITING and STARTED statuses
-    return Games.find({status: {$in: [GameStatuses.WAITING, GameStatuses.STARTED]}});
+    return Games.find({status: {$in: [GameStatuses.WAITING, GameStatuses.STARTED, GameStatuses.FINISHED]}});
   } else {
     return null;
   }
